b4c4229a2e22cc1336d269aba8771d4025e51d1e
yinkalords452-cloud <yinkalords452@gmail.com>
2026-08-15 12:05:39 +0100

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://uqwitdoohutonpxjlxni.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_nvGiGKrmdi4iUSkazSz9xg_SYcV_ECT";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
